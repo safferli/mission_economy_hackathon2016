ed0e81d14bb2a7469d1fb1c00e75a2ea7e6970d3
cluoma <themjolnir999@gmail.com>
2016-06-10 15:35:31 +0200

    Push shiny changes

--- hackathon_shiny/global.R
+++ hackathon_shiny/global.R
@@ -1,4 +1,30 @@
+library(stringi)
 library(shiny)
 library(shinydashboard)
 
 counter = 3
+
+corp_names <- read.csv("names.csv")
+intermed_countries <- read.csv("intermed_countries.csv")
+
+f.generate_random_name <- function(words = 3) {
+  name <- corp_names %>%
+    filter(order <= words | order == 4) %>%
+    group_by(order) %>%
+    sample_n(1)
+  stri_trans_totitle(paste0(name$names, collapse=" "))
+}
+
+f.get_intermediaries <- function(country, rows=5) {
+  ent_setup_count <- intermed_countries %>%
+    filter(grepl(tolower(country), tolower(ent_countries))) %>%
+    ungroup()
+  ent_setup_count$int_address <- mapply(gsub, ent_setup_count$int_name, "", ent_setup_count$int_address)
+  ent_setup_count <- ent_setup_count %>%
+    mutate(int_address = str_trim(int_address)) %>%
+    ungroup() %>% arrange(desc(count)) %>%
+    slice(1:rows) %>%
+    select(Intermediary = int_name, Address = int_address, Country = ent_countries, CorpsSetupPreviously = count) %>%
+    mutate(Address = paste0("<a href=\"https://www.google.ca/maps/search/", Address, "\">", Address, "</a>"))
+}
+
--- hackathon_shiny/server.R
+++ hackathon_shiny/server.R
@@ -13,16 +13,34 @@ library(shiny)
 shinyServer(function(input, output) {
   
   # Panel steps
+  # Name Panel
   output$panel_one <- renderUI({
     conditionalPanel(
       condition = "input.next_button > '-1'",
-      box(as.character(input$next_button))
+      box(
+        title = "Generate your corporation's name:",
+        solidHeader = TRUE,
+        f.generate_random_name(3),
+        width = 12
+      )
     )
   })
   output$panel_two <- renderUI({
     conditionalPanel(
       condition = "input.next_button > '0'",
-      box(as.character(input$next_button))
+      box(
+        title = "Locate your local intermediary:",
+        solidHeader = TRUE,
+        selectInput("country_select", label = "Originating Country:", selected = "",
+                    choices = c("", sort(unique(as.character(intermed_countries$ent_countries))))),
+        conditionalPanel(
+          condition = "input.country_select != ''",
+          renderDataTable({
+            f.get_intermediaries(country = input$country_select, 5)
+          }, escape = FALSE, options = list(paging = FALSE, searching = FALSE))
+        ),
+        width = 12
+      )
     )
   })
   output$panel_three <- renderUI({
